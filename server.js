0917f34096525fc5cf7c9354c1a12916212d8cf5
Salihullah <salihabid56@yahoo.com>
2019-09-13 15:16:20 -0700

    pushing again

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/fru-veg'));
+app.use(express.static(__dirname + '/dist/FruVeg'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/fru-veg/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/FruVeg/index.html'));
 });
 
 // default Heroku PORT
